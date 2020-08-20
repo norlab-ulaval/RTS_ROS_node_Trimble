572422ccad054d830661c95432ef89ecef41494f
Maxime Vaidis <vaidis.maxime@gmail.com>
2020-08-20 13:57:22 -0400

    Add code to process data received and publish them

--- src/theodolite_listener.cpp
+++ src/theodolite_listener.cpp
@@ -44,14 +44,43 @@ void Update_number_theodolite_called(int &number_theodolite, int max_theodolite_
     }
 }
 
+void Read_data(std::string &message_string, bool &corrupted_message, bool &received_data, std::vector<double> &vec_data, int &iterator_vector)
+{
+    std::string single_word_string;
+
+    // looking for the theodolite id number
+    size_t delimiter_position = message_string.find(';');
+    if(delimiter_position == std::string::npos){
+        received_data = true;
+        corrupted_message=true;
+    }
+
+    if(!corrupted_message)
+    {
+        single_word_string = message_string.substr(0, delimiter_position);
+        message_string = message_string.substr(delimiter_position+1, std::string::npos);
+
+        try{
+            vec_data[iterator_vector] = (double) stof(single_word_string);  
+            iterator_vector++;  
+        }
+        catch (const std::invalid_argument & ia){
+            ROS_WARN((std::string("Invalid theodolite id received!")+ia.what()).c_str());
+            received_data = true;
+            corrupted_message=true;                    
+        }
+    }
+}
+
 void Received_data_check(ros::NodeHandle n, ros::Publisher data_pub)
 {
     std::vector<byte> message;
     std::string message_string;
-    std::string single_word_string;
+    //std::string single_word_string;
     unsigned int receivedbytes;
     bool data_CRC_ok = false;
     bool corrupted_message = false;
+    int iterator_vector = 0;
 
     std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
     while(received_data == false)
@@ -68,110 +97,17 @@ void Received_data_check(ros::NodeHandle n, ros::Publisher data_pub)
 
                 if(receivedbytes >= 2)
                 {
-                    // looking for the theodolite id number
-                    size_t delimiter_position = message_string.find(';');
-                    if(delimiter_position == std::string::npos){
-                        received_data = true;
-                        corrupted_message=true;
-                        break;
-                    }
-                    single_word_string = message_string.substr(0, delimiter_position);
-                    message_string = message_string.substr(delimiter_position+1, std::string::npos);
+                    // looking for the theodolite data
 
-                    std::cout << "Word: " << single_word_string << std::endl;
-                    std::cout << "Rest: " << message_string << std::endl;
-
-                    try{
-                        vec_data[0] = (double) stoi(single_word_string);    
-                    }
-                    catch (const std::invalid_argument & ia){
-                        ROS_WARN((std::string("Invalid theodolite id received!")+ia.what()).c_str());
-                        received_data = true;
-                        corrupted_message=true;
-                        break;                    
-                    }
+                    Read_data(message_string, corrupted_message, received_data, vec_data, iterator_vector);
+                    Read_data(message_string, corrupted_message, received_data, vec_data, iterator_vector);
+                    Read_data(message_string, corrupted_message, received_data, vec_data, iterator_vector);
+                    Read_data(message_string, corrupted_message, received_data, vec_data, iterator_vector);
+                    Read_data(message_string, corrupted_message, received_data, vec_data, iterator_vector);
   
-                    std::cout << "Data [0]: " << vec_data[0] << std::endl;
-                                           
-                    /*
-                    ...
-
-                    //Obtain horizontal angle
-                    int new_iterator = 2;
-                    int old_iterator = 2;
-                    while(message[new_iterator]!=';' or new_iterator<(int)receivedbytes)
-                    {
-                        new_iterator+=1;
-                    }
-                    if(new_iterator >= (int)receivedbytes)
-                    {
-                      corrupted_message=true;
-                    }
-                    char* HA_char = new char[new_iterator-old_iterator];
-                    for(int i=0; i<new_iterator-old_iterator; i++)
-                    {
-                        HA_char[i]= (char)message[i+old_iterator];
-                    }
-                    vec_data[1] = atof(HA_char);
-
-                    //Obtain vertical angle
-                    new_iterator += 1;
-                    old_iterator = new_iterator;
-                    while(message[new_iterator]!=';' or new_iterator<(int)receivedbytes)
-                    {
-                        new_iterator+=1;
-                    }
-                    if(new_iterator >= (int)receivedbytes)
-                    {
-                        corrupted_message=true;
-                    }
-                    char* VA_char = new char[new_iterator-old_iterator];
-                    for(int i=0; i<new_iterator-old_iterator; i++)
-                    {
-                        VA_char[i]= (char)message[i+old_iterator];
-                    }
-                    vec_data[2] = atof(VA_char);
-
-                    //Obtain distance
-                    new_iterator += 1;
-                    old_iterator = new_iterator;
-                    while(message[new_iterator]!=';' or new_iterator<(int)receivedbytes)
-                    {
-                       new_iterator+=1;
-                    }
-                    if(new_iterator >= (int)receivedbytes)
-                    {
-                       corrupted_message=true;
-                    }
-                    char* dist_char = new char[new_iterator-old_iterator];
-                    for(int i=0; i<new_iterator-old_iterator; i++)
-                    {
-                       dist_char[i]= (char)message[i+old_iterator];
-                    }
-                    vec_data[3] = atof(dist_char);
-
-                    //Obtain time of data
-                    new_iterator += 1;
-                    old_iterator = new_iterator;
-                    while(message[new_iterator]!=';' or new_iterator<(int)receivedbytes)
-                    {
-                       new_iterator+=1;
-                    }
-                    if(new_iterator >= (int)receivedbytes)
-                    {
-                       corrupted_message=true;
-                    }
-                    char* time_char = new char[new_iterator-old_iterator];
-                    for(int i=0; i<new_iterator-old_iterator; i++)
-                    {
-                       time_char[i]= (char)message[i+old_iterator];
-                    }
-                       vec_data[4] = atof(time_char);
-
-
                     std_msgs::Float64MultiArray msg;
                     msg.layout.dim.push_back(std_msgs::MultiArrayDimension());
-                    msg.layout.dim[0].size = 7;
+                    msg.layout.dim[0].size = 5;
                     msg.layout.dim[0].stride =1;
 
                     vector<double>::const_iterator itr, end(vec_data.end());
@@ -181,12 +117,11 @@ void Received_data_check(ros::NodeHandle n, ros::Publisher data_pub)
 
                     if(show_data)
                     {
-                       ROS_INFO("theodolite: %f \n HA: %f \n VA: %f \n Distance: %f \n Time: %f \n", vec_data[0], vec_data[1], vec_data[2], vec_data[3], vec_data[4]);
+                       ROS_INFO("theodolite: %f ; HA: %f ; VA: %f ; Distance: %f ; Time: %f \n", vec_data[0], vec_data[1], vec_data[2], vec_data[3], vec_data[4]);
                     }
 
-                    data_pub.publish(msg);           */           
+                    data_pub.publish(msg);                        
                 }
-                received_data = true;
             }
             else{
                     ROS_WARN("Received corrupted message (bad CRC)");
@@ -244,7 +179,7 @@ int main(int argc, char **argv)
     int max_theodolite_number = number_of_theodolite;
 
     // Delay parameter in milliseconds between each message sent
-    int time_delay = 30;
+    int time_delay = 10;
 
     //Listen messages sent
     while (ros::ok())
